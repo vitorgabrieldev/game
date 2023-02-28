ab28a81ece9fc24f1b046e562dd741a220905b0e
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-02-27 22:18:44 -0300

    BH

--- transition.js
+++ transition.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
         move_pages.section_left();
     }, 4900);
 });
-
 const move_pages = {
     section_left: function() {
         document.querySelector('#container_page_logo').classList.add("move_left");
@@ -11,13 +10,23 @@ const move_pages = {
     },
     two_page_settings: function() {
         typeWriter(document.querySelector('.titleMessage_twoPage'));
+        setTimeout(() => {
+            document.querySelector('#container_page_messages_alert').classList.add("move_left");
+            this.section_right();
+        }, 8000);
+    },
+    section_right:function() {
+
     },
 };
-
 function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
       setTimeout(() => elemento.innerHTML += letra, 75 * i);
     });
+};
+
+function enterMouseButton(txt) {
+    document.querySelector('.titleOPtionHover').innerHTML = txt;
 };
